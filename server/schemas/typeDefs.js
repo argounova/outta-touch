8a7aab564a4e4ca3cc212f8c58f5e9b20ba2fc22
Brandon <hillbp2014@gmail.com>
2022-12-13 10:25:32 -0500

    created typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,30 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type User {
-    _id: ID!
-    username: String!
-    email: String!
+    _id: ID
+    username: String
+    email: String
+    groups: [Group]
+  }
+
+  type Group {
+    _id: ID
+    name: String
+    members: [
+      {
+        user: User
+        isAdmin: Boolean
+      }
+    ]
+    messages: [
+      {
+        message: String
+        user: User
+        timestamp: Date
+        updated: Boolean
+      }
+    ]
   }
 
   type Auth {
@@ -14,6 +34,7 @@ const typeDefs = gql`
 
   type Query {
     user(userId: ID!): User
+    group(groupId: ID!): Group
   }
 
   type Mutation {
