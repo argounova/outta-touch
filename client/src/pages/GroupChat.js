cec77a2600d12e1a32b02a579b51b688e6146554
Volantis <putzstuck@gmail.com>
2022-12-13 13:00:37 -0500

    added basic code structure to file

---
+++ client/src/pages/GroupChat.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import AuthService from '../utils/auth';
+
+
+// components
+import NavBar from '../components/NavBar';
+
+// import css file
+import './assets/css/groupchat.css';
+
+const GroupChat = () => {
+
+    const logoutClick = () => {
+
+    }
+
+    const photosClick = () => {
+
+    }
+
+    const dashboardClick = () => {
+
+    }
+
+    const MainContent = () => {
+        return (
+            <>
+
+            </>
+        )
+    }
+
+    return (
+        <>
+        <header>
+            <NavBar />
+        </header>
+        <main>
+            <section>
+
+            </section>
+        </main>
+        </>
+    )
+}
+
+export default GroupChat;
