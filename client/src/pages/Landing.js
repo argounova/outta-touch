1be6ee18261810031c22ec037724b35e19d3c1cd
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-13 00:46:22 -0500

    Log in and sign up buttons added

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 
+// import css file
+import './assets/css/landing.css';
+
 const Landing = () => {
   return (
-    <main>
-      <NavBar slogan='Are You Outta Touch?'/>
+    <>
+    <header>
+    <NavBar slogan='Are You Outta Touch?'/>
+    </header>
+    <main className='landing-main'>
+      <section className='landing-container'>
+      <Link className='landing-text' to='/login'>
+      Log In
+      </Link>
+      <Link className='landing-text' to='/signup'>
+      SignUp
+      </Link>
+      </section>
     </main>
+    </>
   )
 };
 
