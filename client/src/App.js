31b35f7efa5ddd8433c858f74a2bb4690a9dddb8
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-13 11:49:38 -0500

    updated functionality of protect route component, if user tries to access the dashboard route, they are presented with an error message if they are not logged in and given the option to return to the home screen

--- client/src/App.js
+++ client/src/App.js
@@ -47,7 +47,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Landing />} />
           <Route
-            path='dashboard/:userId'
+            path='/dashboard/:userId'
             element={Auth.loggedIn() ? <Dashboard /> : <ProtectRoute />}
           />
         </Routes>
--- client/src/components/ProtectRoute/index.js
+++ client/src/components/ProtectRoute/index.js
@@ -1,37 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { Container, Typography, Button } from '@mui/material'
 
-import padlock from './assets/images/padlock.png';
+import logo from './assets/images/HomeButtonImage.png';
+
+import './assets/css/protectroute.css';
 
 const ProtectRoute = () => {
   return (
-    <main>
-      <Container sx={{
-        height: '75vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        <img src={padlock} alt='padlock' style={{ height: '250px', width: '250px', margin: 3 }} />
-        <Container sx={{ m: 3 }}>
-          <Typography variant='h4' align='center'>Sorry, but you must be logged in to your account to view this information.</Typography>
-        </Container>
-        <Container sx={{
-          m: 3,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-          <Link to='/login' style={{ textDecoration: 'none' }}>
-            <Button variant='outlined' sx={{ m: 1 }}>Login</Button>
-          </Link>
-          <Link to='/signup' style={{ textDecoration: 'none' }}>
-            <Button variant='outlined' sx={{ m: 1 }}>Sign Up</Button>
-          </Link>
-        </Container>
-      </Container>
+    <main className='protect-main'>
+        <Link to='/'>
+          <img className='protect-logo' src={logo} />
+        </Link>
+          <h1 className='error-msg'>Sorry, but you must be logged in to your account to view this information.</h1>
     </main>
   )
 };
