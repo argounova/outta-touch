16b55a1081ee8e5d590ff7a484d66f1e6ac16d7a
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-14 00:40:22 -0500

    nav hamburger menu and logout custom buttons added

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -7,22 +7,30 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavBar from '../components/NavBar';
 import './assets/css/dashboard.css';
 
+// get custom hamburger nav icon
+import hamburgerIcon from './assets/img/HamburgerIcon.png';
+import logOutIcon from './assets/img/LogOut.png';
+
 const Dashboard = () => {
   return (
     <>
+    <header className='dashboard-header'>
       <NavBar navToggle={
         <>
           <Navbar.Toggle className='toggle-button' aria-controls="custom-collapse">
-            X
-            </Navbar.Toggle>
+            <img className='toggle-icon' src={hamburgerIcon} />
+          </Navbar.Toggle>
           <Navbar.Collapse id="custom-collapse">
             <Nav className="me-auto">
-              <Nav.Link href="/">Sign Out</Nav.Link>
+              <Nav.Link href="/">
+                <img className='custom-nav-icons' src={logOutIcon} />
+                </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </>
       } />
-      <main>
+      </header>
+      <main className='dashboard-main'>
         <h1>Dashboard</h1>
         <button onClick={AuthService.logout}>Logout</button>
       </main>
