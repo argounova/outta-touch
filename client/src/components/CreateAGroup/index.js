06b02fdd9d40f8d971f58037c45700848f6d9f7c
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-14 12:46:22 -0500

    bolerplate submission form added to create a group component

--- client/src/components/CreateAGroup/index.js
+++ client/src/components/CreateAGroup/index.js
@@ -1,8 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from '@apollo/client';
+
+// CSS
+import './assets/css/createagroup.css';
 
 const CreateAGroup = () => {
+    const [formState, setFormState] = useState({ groupName: '', password: '' });
+    const [createGroup, { error, data }] = useMutation(CREATE_GROUP);
+
+    /// HANDLE CHANGE ///
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    /// FORM SUBMISSION ///
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await createGroup({
+        variables: { ...formState },
+      });
+
+    } catch (error) {
+      console.log(error);
+    }
+
+    setFormState({
+      groupName: '',
+      password: '',
+    });
+  };
+
+
     return (
-        <h1>This is the CreateAGroup Component</h1>
+        <>
+            <h1 className='create-title'>Create A Group</h1>
+            <form className='custom-form' onSubmit={handleFormSubmit}>
+                <input
+                    placeholder='Enter Group Name'
+                    name='groupName'
+                    type='text'
+                    value={formState.groupName}
+                    onChange={handleChange}
+                />
+                <input
+                    placeholder='Enter Group Password'
+                    name='password'
+                    type='password'
+                    value={formState.password}
+                    onChange={handleChange}
+                />
+                <button className='create-button' type='submit'>Create Group</button>
+            </form>
+            {error && (
+                <div>{error.message}</div>
+            )}
+        </>
     )
 }
 
