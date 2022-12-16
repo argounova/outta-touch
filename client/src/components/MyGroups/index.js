2a3388e012644ce1c719dcf65248e434fca0e1c1
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-15 23:31:31 -0500

    got distracted, styled buttons for groups tee hee

--- client/src/components/MyGroups/index.js
+++ client/src/components/MyGroups/index.js
@@ -9,6 +9,13 @@ import Auth from "../../utils/auth";
 // import queries
 import { QUERY_USER } from "../../utils/queries";
 
+// import images
+import bricksImg from './assets/img/bricks.jpg';
+import paintSplatter10Img from './assets/img/paintsplatter10.jpg';
+import paintSwirlImg from './assets/img/paintswirl.jpg';
+import paintSwirl2Img from './assets/img/paintswirl2.jpg';
+import paintSwirl3Img from './assets/img/paintswirl3.jpg';
+
 // MyGroups Component
 const MyGroups = () => {
     // get user information
@@ -39,19 +46,31 @@ const MyGroups = () => {
         userGroupsIds.push(userGroups[i]._id);
     }
 
+    // array of images
+    const images = [bricksImg, paintSplatter10Img, paintSwirlImg, paintSwirl2Img, paintSwirl3Img];
+
+    const random = Math.floor(Math.random() * images.length);
+
+    // set button style here instead of css so we can be dynamic
+    const buttonStyle = {
+        backgroundImage: `url(${images[random]})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+    }
+
     // in the return we will map over the information and since the index length will be the same for both arrays, it makes it that much easier to get the information we need!
-
-
+// TODO: render a message that tells user to head to create a group if they do not have any created
     return (
         <>
             <h1 className="myGroups-title">{loading ? (
                 <div>Loading...</div>
             ) : `${data.user.username}'s Groups`}</h1>
-            <section>
+            <section className="groups-section">
             {loading ? (
                 <div>Loading...</div>
             ) : userGroupsNames.map((names, index) =>
-                <button data-group-id={userGroupsIds[index]} key={names.replace(/\s/g, "")}>{names}</button>
+                <button style={buttonStyle} className="groups-button" data-group-id={userGroupsIds[index]} key={names.replace(/\s/g, "")}>{names}</button>
             )}
             </section>
         </>
