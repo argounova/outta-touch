9a16e89f60c0ce99ac5e2312567965fb95c8bd32
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-16 12:43:16 -0500

    correctly identifying if user is the admin to the group

--- client/src/components/LiveChat/index.js
+++ client/src/components/LiveChat/index.js
@@ -4,9 +4,12 @@ import './assets/css/livechat.css';
 // import Photos from "../Photos";
 
 import { QUERY_GROUP } from '../../utils/queries';
-import { useQuery } from '@apollo/client';
+import { ADD_GROUP_MEMBER } from "../../utils/mutations";
+import { useQuery, useMutation } from '@apollo/client';
 
 import inviteBtn from './assets/img/Invite.png';
+
+import Auth from "../../utils/auth";
 
 const LiveChat = () => {
     // const navigate = useNavigate();
@@ -18,7 +21,58 @@ const LiveChat = () => {
             }
         });
 
-    console.log(loading ? 'Loading ...' : data.group.name);
+    // console.log(loading ? 'Loading ...' : data.group.admins);
+    let adminArray = loading ? 'Loading ...' : data.group.admins;
+
+    console.log(adminArray);
+
+    const AddGroupMember = () => {
+        const [addGroupMember, { error, data }] = useMutation(ADD_GROUP_MEMBER);
+        const currentUser = Auth.getUser();
+        const currentGroup = localStorage.getItem('currentGroupChat');
+
+        function evaluateClick() {
+            for (let i = 0; i < adminArray.length; i++) {
+                console.log(adminArray[i]._id);
+                console.log(currentUser.data._id);
+                if (adminArray[i]._id.includes(currentUser.data._id)) {
+                    alert('Sick Dude')
+                }
+                else {
+                    alert('You must be an admin to add a group member')
+                }
+            }
+        }
+
+        // try {
+        //     const { data } = addGroupMember({
+        //         variables: {
+        //             userId: '2331423dwed23',
+        //             groupId: 'lajsflkf3k32kd',
+        //             admin: 'asdfsdfoe3'
+        //         },
+        //     });
+
+        // } catch (e) {
+        //     console.error(e);
+        // }
+
+        return (
+            <button className='invite-btn' onClick={evaluateClick}>
+                <img className='invite-icon' src={inviteBtn} alt='invite button' />
+            </button>
+        )
+
+    }
+
+// ! have two handlers? One if admin matches user id and on if not?
+    const successMemberClick = () => {
+        
+    }
+
+    const failureMemberClick = () => {
+
+    }
 
     return (
         <>
@@ -27,9 +81,7 @@ const LiveChat = () => {
                 <h2> {loading ? (<div>Loading...</div>) : data.group.name} </h2>
                 {/* TODO: Add logo button for photos */}
                 {/* <button onClick={ () => navigate(<Photos/>) }>PHOTOS</button> */}
-                <button className='invite-btn'>
-                    <img className='invite-icon' src={inviteBtn} alt='invite button' />
-                </button>
+                <AddGroupMember />
             </div>
             <div className='message-div'>
                 {/* TODO: Render messages in real time via subscriptions */}
