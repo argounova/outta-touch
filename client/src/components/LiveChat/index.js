2d83d67ba1cefeff2da85f79c7d2a82c46476afe
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-16 11:23:30 -0500

    Invite Icon added

--- client/src/components/LiveChat/index.js
+++ client/src/components/LiveChat/index.js
@@ -6,6 +6,8 @@ import './assets/css/livechat.css';
 import { QUERY_GROUP } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
 
+import inviteBtn from './assets/img/Invite.png';
+
 const LiveChat = () => {
     // const navigate = useNavigate();
 
@@ -16,23 +18,26 @@ const LiveChat = () => {
             }
         });
 
-        console.log(loading ? 'Loading ...' : data.group.name);
+    console.log(loading ? 'Loading ...' : data.group.name);
 
     return (
         <>
             <div className='chat-name'>
-            {/* TODO: Add dynamic chat name */}
-                <h2> { loading ? ( <div>Loading...</div> ) : data.group.name } </h2>
-            {/* TODO: Add logo button for photos */}
+                {/* TODO: Add dynamic chat name */}
+                <h2> {loading ? (<div>Loading...</div>) : data.group.name} </h2>
+                {/* TODO: Add logo button for photos */}
                 {/* <button onClick={ () => navigate(<Photos/>) }>PHOTOS</button> */}
+                <button className='invite-btn'>
+                    <img className='invite-icon' src={inviteBtn} alt='invite button' />
+                </button>
             </div>
             <div className='message-div'>
-            {/* TODO: Render messages in real time via subscriptions */}
+                {/* TODO: Render messages in real time via subscriptions */}
             </div>
             {/* TODO: Add button send message functionality */}
             <form className="message-form">
-            <input className="message-input" placeholder=". . . my super sick message" name="chat" type="text" />
-            <button className='message-button' type="submit" >Send Message</button>
+                <input className="message-input" placeholder=". . . my super sick message" name="chat" type="text" />
+                <button className='message-button' type="submit" >Send Message</button>
             </form>
         </>
     )
