edaed95b1cac7ff2d98658a8494f5c9f691d6a52
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-17 21:42:51 -0500

    when page is loaded, all messages from that group show on the page

--- client/src/components/UserMessages/index.js
+++ client/src/components/UserMessages/index.js
@@ -1,11 +1,44 @@
 import React, { useState } from "react";
-import './assets/css/usermessages.css'
+import './assets/css/usermessages.css';
+import { useQuery } from '@apollo/client';
+import { QUERY_GROUP } from '../../utils/queries';
+
 
 const UserMessages = () => {
 
     const currentGroup = localStorage.getItem('currentGroupChat');
     const currentUser = localStorage.getItem('currentUser');
-    const [formState, setFormState] = useState({ message: ''});
+    const [formState, setFormState] = useState({ message: '' });
+
+    const { loading, data } = useQuery(QUERY_GROUP,
+        {
+            variables: {
+                groupId: currentGroup
+            }
+        })
+
+    const messageData = loading ? 'Loading...' : data.group.messages;
+    console.log(messageData);
+
+    const handleMessages = Object.keys(messageData).map((index) => {
+
+        if (loading) {
+            return (
+                <div>Loading...</div>
+            )
+        } else {
+            return(
+                <ul className="user-messages" key={index}>
+                <li className="message-username">{messageData[index].user.username}</li>
+                <li className="message-body">{messageData[index].body}</li>
+                <li className="message-timestamp">sent: {messageData[index].timestamp}</li>
+                </ul>
+            )
+        }
+
+    });
+
+
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -34,6 +67,7 @@ const UserMessages = () => {
             <div className='message-div'>
                 {/* TODO: Render messages in real time via subscriptions */}
                 <h2 id="user-heading">Logged in as: {currentUser}</h2>
+                    {loading ? 'Loading..' : handleMessages}
             </div>
             {/* TODO: Add button send message functionality */}
             <form className="message-form" onSubmit={handleFormSubmit}>
